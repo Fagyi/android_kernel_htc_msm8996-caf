ab94773bc58d65171567adcf921428f8c6d499a3
joshuous <joshuous@gmail.com>
2018-06-12 15:38:25 +0000

    Initial AudioWizard bringup
    Ported from Asus ZS551KL kernel source.
    
    Signed-off-by: joshuous <joshuous@gmail.com>
    
    Signed-off-by: Laster K. (lazerl0rd) <officiallazerl0rd@gmail.com>

--- sound/soc/codecs/wcd-mbhc-v2.h
+++ sound/soc/codecs/wcd-mbhc-v2.h
@@ -558,6 +558,8 @@ struct wcd_mbhc {
 	__ATTR(flag, _mode, _show, _store)
 //HTC_AUD_END
 
+void wcd_mbhc_plug_detect_for_debug_mode(struct wcd_mbhc *mbhc, int debug_mode); /* ASUS_BSP Paul +++ */
+
 #ifdef CONFIG_SND_SOC_WCD_MBHC
 int wcd_mbhc_set_keycode(struct wcd_mbhc *mbhc);
 int wcd_mbhc_start(struct wcd_mbhc *mbhc,
