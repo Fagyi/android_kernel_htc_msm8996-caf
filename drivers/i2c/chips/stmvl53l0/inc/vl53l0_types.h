de9269f649e190963fb0dc50303dd7d36aa2e6f6
Fagyi <fagyipda@gmail.com>
2021-02-21 22:49:11 +0100

    Drivers/chips: remove redefinition
    drivers/i2c/chips/stmvl53l0/inc/vl53l0_types.h:94:23: warning: redefinition of typedef 'uint8_t' is a C11 feature [-Wtypedef-redefinition]
    typedef unsigned char uint8_t;
                          ^
    include/linux/types.h:106:16: note: previous definition is here
    typedef         __u8            uint8_t;
                                    ^

--- drivers/i2c/chips/stmvl53l0/inc/vl53l0_types.h
+++ drivers/i2c/chips/stmvl53l0/inc/vl53l0_types.h
@@ -50,59 +50,6 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #error "Error NULL definition should be done. Please add required include "
 #endif
 
-
-//#if ! defined(STDINT_H) &&  !defined(_GCC_STDINT_H) &&!defined(__STDINT_DECLS) && !defined(_GCC_WRAP_STDINT_H)
-
-// #pragma message("Please review  type definition of STDINT define for your platform and add to list above ")
-
- /*
-  *  target platform do not provide stdint or use a different #define than above
-  *  to avoid seeing the message below addapt the #define list above or implement
-  *  all type and delete these pragma
-  */
-
-/** \ingroup VL53L0_portingType_group
- * @{
- */
-
-
-typedef unsigned long long uint64_t;
-
-
-/** @brief Typedef defining 32 bit unsigned int type.\n
- * The developer should modify this to suit the platform being deployed.
- */
-typedef unsigned int uint32_t;
-
-/** @brief Typedef defining 32 bit int type.\n
- * The developer should modify this to suit the platform being deployed.
- */
-typedef int int32_t;
-
-/** @brief Typedef defining 16 bit unsigned short type.\n
- * The developer should modify this to suit the platform being deployed.
- */
-typedef unsigned short uint16_t;
-
-/** @brief Typedef defining 16 bit short type.\n
- * The developer should modify this to suit the platform being deployed.
- */
-typedef short int16_t;
-
-/** @brief Typedef defining 8 bit unsigned char type.\n
- * The developer should modify this to suit the platform being deployed.
- */
-typedef unsigned char uint8_t;
-
-/** @brief Typedef defining 8 bit char type.\n
- * The developer should modify this to suit the platform being deployed.
- */
-typedef signed char int8_t;
-
-/** @}  */
-//#endif /* _STDINT_H */
-
-
 /** use where fractional values are expected
  *
  * Given a floating point value f it's .16 bit point is (int)(f*(1<<16))*/
